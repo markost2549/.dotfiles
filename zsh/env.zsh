6317f76f800a122e53eace9224a37f5d825bffbe
Marko <markost2549@gmail.com>
2026-05-03 15:07:58 +0200

    feat: set STARSHIP_CONFIG for gruvbox-rainbow theme in Zsh environment

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,3 +4,5 @@ export NODE_OPTIONS="--max-old-space-size=4096"
 # NVM (lazy-ish load)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+export STARSHIP_CONFIG=~/.config/gruvbox-rainbow.toml
