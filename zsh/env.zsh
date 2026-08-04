dd1cfb8d3d89cc914997d42a9801f1c3b0eb6903
Marko Stojkovic <markost2549@gmail.com>
2026-08-04 23:05:27 +0200

    fix: move NVM loading to env.zsh for better organization

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,6 +3,8 @@ export NODE_OPTIONS="--max-old-space-size=4096"
 
 # NVM (lazy-ish load)
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 export STARSHIP_CONFIG=~/.config/gruvbox-rainbow.toml
