4e68deecf1c8a9b26c6ed3e26f3b3015923288cb
Marko Stojkovic <markost2549@gmail.com>
2026-08-12 18:44:06 +0200

    feat: update environment configuration to include plain text symbols for Starship

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -7,6 +7,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 
+# Gruvbox Rainbow theme
 export STARSHIP_CONFIG=~/.config/gruvbox-rainbow.toml
+
+# No Nerd font symbols
 # export STARSHIP_CONFIG=~/.config/no-nerd-font.toml
+
+# Nerd font symbols
 # export STARSHIP_CONFIG=~/.config/nerd-font-symbols.toml
+
+# Plain text symbols
+# export STARSHIP_CONFIG=~/.config/plain-text-symbols.toml
